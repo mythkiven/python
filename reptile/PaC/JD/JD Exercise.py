46900a770d2f5c64b5df602322fdb2167c766ca7
gyjrong <1282412855@qq.com>
2017-06-01 15:04:41 +0800

    new

---
+++ reptile/PaC/JD/JD Exercise.py
@@ -0,0 +1,20 @@
+# encoding:utf-8
+
+
+# 导入模块:
+
+import requests #网络模块
+import os   #系统模块
+import time # 日期模块
+import json # json 解析
+import random #随机数模块
+
+#定义JD类 扫码登录类:
+
+class JDLoginByQR():
+
+    def __init__(self):
+
+        self.request =
+
+        
